14a724c77b2ce0d50470ed6e8d78311332ac2c6f
felipeneneu <feliperpg3@gmail.com>
2024-04-08 18:25:30 -0300

    first-api-conversor

---
+++ scripts.js
@@ -0,0 +1,53 @@
+const button = document.getElementById("convert-Button")
+const select = document.getElementById("select-currency")
+
+
+const dolar = 5.03
+const euro = 5.46
+
+
+const convertValues = () => {
+    const inputReais = document.getElementById("input-convert").value
+    const realValueText = document.getElementById("realValueText")
+    const dolarValueText = document.getElementById("dolarValueText")
+
+    //realValueText.innerHTML = inputReais
+    //dolarValueText.innerHTML = inputReais / dolar
+
+    realValueText.innerHTML = new Intl.NumberFormat('pt-BR',
+        { style: 'currency', currency: 'BRL' }
+    ).format(inputReais)
+
+    if (select.value === 'US$ Dólar americano') {
+        dolarValueText.innerHTML = new Intl.NumberFormat('en-US',
+            { style: 'currency', currency: 'USD' }
+        ).format(inputReais / dolar)
+    }
+    if (select.value === '€ Euro') {
+        dolarValueText.innerHTML = new Intl.NumberFormat('de-DE',
+            { style: 'currency', currency: 'EUR' }
+        ).format(inputReais / euro)
+    }
+}
+
+chanceCurrency = () => {
+    const sectionName = document.getElementById('section-name')
+    const currencyImg = document.getElementById('currency-img')
+
+
+    if (select.value === 'US$ Dólar americano') {
+        sectionName.innerHTML = 'US$ Dólar americano'
+        currencyImg.src = './assets/estados-unidos (1) 1.svg'
+    }
+
+    if (select.value === '€ Euro') {
+        sectionName.innerHTML = 'Euro'
+        currencyImg.src = './assets/Design sem nome 1.svg'
+    }
+
+    convertValues()
+}
+
+button.addEventListener('click', convertValues)
+select.addEventListener('change', chanceCurrency)
+console.log(button)
